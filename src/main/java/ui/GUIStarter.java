7c104cb59a5b4fc3884f8fe66d3f0932dc54c4d4
Thomas GREGOIRE <thomas.gregoire82@gmail.com>
2022-04-20 10:33:52 +0200

    Modification mineures

---
+++ src/main/java/ui/GUIStarter.java
@@ -0,0 +1,16 @@
+package ui;
+
+public class GUIStarter {
+
+
+    /*
+        NE PAS SUPPRIMER
+        CETTE CLASSE PERMET DE LANCER L'APPLICATION A PARTIT D'UN JAR 
+        AFIN DE CREE UN EXECUTABLE
+    */
+
+    public static void main(final String[] args){
+        App.main(args);
+    }
+    
+}
