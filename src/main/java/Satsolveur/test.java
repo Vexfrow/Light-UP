b82ccdd9478468284760e03f02fe50db04231fd3
fabien.vial@etu.univ-grenoble-alpes.fr <fabien.vial@etu.univ-grenoble-alpes.fr>
2022-04-21 10:15:22 +0200

    ajout du Satsolveur afin de pouvoir l'observer avec d'autre point de vue

---
+++ src/main/java/Satsolveur/test.java
@@ -0,0 +1,16 @@
+public class test {
+    public static void main(String[] args){
+        String st = "Une chaine pleine";
+        String ts = "-5";
+        String[] ligne = ts.split(" ");
+        int i = 0;
+        System.out.println("taille = " + ligne.length);
+        while(i < ligne.length){
+            System.out.println(ligne[i]);
+            i++;
+        }
+        
+    }
+
+
+}
