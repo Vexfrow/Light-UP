83af92fd663cbfb04552b7044cda688a2ee1401e
Thomas GREGOIRE <thomas.gregoire82@gmail.com>
2022-06-26 12:24:46 +0200

    Ajustement au niveau de l'IHM

--- src/main/java/ui/WelcomeController.java
+++ src/main/java/ui/WelcomeController.java
@@ -4,7 +4,9 @@ import java.io.IOException;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Alert;
+import javafx.scene.control.Label;
 import javafx.scene.control.Alert.AlertType;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
 /*
@@ -17,7 +19,11 @@ public class WelcomeController
     /*---------------------------- ATTRIBUTS ----------------------------*/
 
     /* a) RECUPERATION DE L'ATTRIBUT STAGE */
-    Stage stage = App.stage; 
+    Stage stage = App.stage;
+    
+    /* b) RECUPERATION DANS LE FXML "welcome.fxml" */
+    @FXML
+    Label text_welcome;
 
     /*---------------------------- METHODES -----------------------------*/
 
@@ -70,5 +76,6 @@ public class WelcomeController
         stage.setWidth(1000);
         stage.setResizable(false);
         stage.centerOnScreen();
+        text_welcome.setText("Ce programme a été réalisé dans le cadre d'un projet étudiant à l'UGA.\n\nLes auteurs sont : \n\t • Thomas Grégoire, \n\t • Vial Fabien, \n\t • Bolbènes Lucas, \n\t • Combe Gaëtan, \n\t • El Kbabty Mouad. ");
     }
 }
